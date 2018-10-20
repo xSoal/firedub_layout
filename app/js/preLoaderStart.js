b2a3ae4c12077e52de0dfb00f48ff25f9941c4bf
xSoal <xsoal@mail.ru>
2018-10-20 13:35:18 +0300

    fix preloader video

--- app/js/preLoaderStart.js
+++ app/js/preLoaderStart.js
@@ -17,34 +17,34 @@ let e  = () => {
 		if( document.querySelector('.pre-loader__img-cont') != null ){
 			videoCont = document.querySelector('.pre-loader__img-cont');
 			clearInterval(timer);
+
 			initPreVideo();
 		}
 	},50);
 
 	function initPreVideo () {
 		let video = document.createElement('video');
-
 		video.src = 'images/video/fire_short_quality.mp4';
-
-	    video.addEventListener('canplay', function () {
-	       canPlay.call(video);
-	    }, false);
-
-
-		function canPlay() {
-
-			this.style.opacity = 1;
-			this.play();
-			videoCont.appendChild(this);
-			
-
-		}
-
-
-
-
 		video.classList.add('pre-loader__img');
-		video.classList.add('hiden');
+		videoCont.appendChild(video);
+		video.preload = "auto";
+		video.load();
+		
+	    // video.addEventListener('canplay', function () {
+	    //    canPlay.call(video);
+	    // }, false);
+
+
+		let timer = setInterval( () => {
+			console.log( video.readyState, 'READY STATE VIDEO' )
+			if( video.readyState == 4 ) {
+				clearInterval(timer);
+				video.style.opacity = 1;
+				video.play();
+			}
+			
+		}, 25 )
+
 		
 
 	}
